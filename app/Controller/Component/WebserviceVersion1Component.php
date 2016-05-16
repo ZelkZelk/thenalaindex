ab80b98cc2aac923553a5c122f436bae6f0fb63d
klez <felipeklez@gmail.com>
2016-05-16 11:40:08 -0400

    Empezando el webservice, se agregaron componentes

---
+++ app/Controller/Component/WebserviceVersion1Component.php
@@ -0,0 +1,60 @@
+<?php
+
+class WebserviceVersion1Component extends Component{
+    private $Controller;
+    
+    public function initialize(\Controller $controller) {
+        $this->Controller = $controller;
+        return parent::initialize($controller);
+    }
+    
+    /** Obtiene los Targets habilitados, ordenados por nombre */
+    
+    public function targets(){
+        App::import('Model','Target');
+        $Target = new Target();
+        $targets = $Target->findTargets();
+        $alias = $Target->alias;
+        $output = [];
+        
+        foreach($targets as $target){
+            $output[] = [
+                'url' => $target[$alias]['url'],
+                'name' => $target[$alias]['name']
+            ];
+        }
+                
+        $this->Controller->pushOutput($output);
+    }
+    
+    /** 
+     * Obtiene los historiales por sitio.
+     **/
+    
+    public function histories(){
+        App::import('Model','Target');
+        $Target = new Target();
+        $id = $this->Controller->getWebserviceData('target_id');
+        
+        if($Target->loadFromId($id)){
+            $limit = $this->Controller->getWebserviceData('limit');
+            $page = $this->Controller->getWebserviceData('page');
+        
+            App::import('Model','CrawlerLog');
+            $CrawlerLog = new CrawlerLog();
+            $logs = $CrawlerLog->findPagedByTarget($id,$page,$limit);
+            $alias = $CrawlerLog->alias;
+            
+            $output = [];
+            
+            foreach($logs as $log){
+                $output[] = $log[$alias];
+            }
+            
+            $this->Controller->pushOutput($output);
+        }
+        else{
+            throw new NotFoundException("Target@{$id} not found");
+        }
+    }
+}
---
+++ app/Controller/WebserviceController.php
@@ -0,0 +1,207 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+class WebserviceController extends Controller {    
+    private $output = [];
+    
+    public function deleteOutput($key = null){
+        if(is_null($key)){
+            $this->output = [];
+            return;
+        }
+        
+        unset($this->output[$key]);
+    }
+    
+    public function pushOutput($data, $key = null){
+        if(is_null($key)){
+            $this->output = $data;
+            return;
+        }
+        
+        $this->output[$key] = $data;
+    }
+    
+    /* Este webservice devuelve siempre un JSON */
+    
+    public function beforeRender() {
+        header('Content-Type: application/json');
+        
+        ob_clean();
+        echo json_encode($this->output);
+        ob_flush();
+        
+        exit;
+    }
+    
+    /* Basado en los parametros enrutados redirige el flujo al metodo
+     * 
+     * $version = $this->version['version'];
+     * $webservice = $this->version['webservice'];
+     * 
+     * {$webservice}_v{$version}
+     * 
+     * Si dicho metodo no existe se devuelve un status code 404
+     */
+    
+    private $Webservice;
+    
+    public function index(){
+        $this->version = $this->params['version'];
+        $this->webservice = $this->params['webservice'];
+        $this->extra = $this->params['extra'];
+        
+        $component = "WebserviceVersion{$this->version}Component";
+        $method = "{$this->webservice}";
+        
+        App::import('Controller/Component',$component);
+        
+        if(class_exists($component) === false){
+            throw new NotFoundException("Class $component not found");
+        }
+        
+        $collection = new ComponentCollection();
+        $this->Webservice = new $component($collection);
+        $this->Webservice->initialize($this);
+        
+        if(method_exists($this->Webservice, $method)){
+            if($this->parseConf()){
+                $this->Webservice->$method();
+            }
+            else{
+                throw new ForbiddenException();
+            }
+        }
+        else{
+            throw new NotFoundException("$method() not found");
+        }
+    }
+    
+    /** Parsea el archivo de configuracion y determina si los parametros pasados 
+     * son validos.
+     */
+    
+    private $get = false;
+    private $post = false;
+    private $schema = [];
+    
+    private function parseConf(){
+        Configure::load('webservice');
+        $key = "v{$this->version}.{$this->webservice}";
+        $conf = Configure::read($key);
+        
+        if($conf){
+            $this->get = (boolean) @$conf['get'];
+            $this->post = (boolean) @$conf['post'];
+            $this->schema = (array) @$conf['data'];
+        }
+        
+        return $this->analyzeConf();
+    }
+    
+    /** Analiza la configuracion y determina si es valida. */
+    
+    private function analyzeConf(){
+        $isGet = $this->request->is('get');
+        $isPost = $this->request->is('post');
+        
+        if($isGet){
+            return $this->analyzeGet();
+        }
+        
+        if($isPost){
+            return $this->analyzePost();
+        }
+        
+        return false;
+    }
+    
+    /** Analiza la peticion GET */
+    
+    private function analyzeGet(){
+        if($this->get){
+            $this->webserviceData = $this->params->query;
+            return $this->analyzeData($this->data);            
+        }
+        
+        return false;
+    }
+    
+    /** Analiza la peticion POST */
+    
+    private function analyzePost(){
+        if($this->post){
+            $this->webserviceData = $this->data;
+            return $this->analyzeData();
+        }
+        
+        return false;
+    }
+    
+    /* Analiza los datos enviados */
+    
+    private $webserviceData = [];
+    
+    private function analyzeData(){
+        foreach($this->schema as $key => $data){
+            $exists = false;
+            $required = (boolean) @$data['required'];
+            $readonly = (boolean) @$data['readonly'];
+            $type = (string) @$data['type'];
+            $value = @$data['default'];
+            
+            if(isset($this->webserviceData[$key]) && $readonly === false){
+                $value = $this->webserviceData[$key];
+                $exists = true;
+            }
+            
+            if($required && $exists === false){
+                throw new BadRequestException("Debe especificarse parametro $key");
+            }
+            
+            list($newValue,$typeResult) = $this->analyzeType($value,$type);
+            
+            if($typeResult == false){
+                throw new BadRequestException("Parametro $key debe ser $type");
+            }
+            
+            $this->webserviceData[$key] = $newValue;
+        }
+        
+        return true;
+    }
+    
+    /** Analiza el tipo de dato y devuelve un flag que determina si es correcto o no */
+    
+    private function analyzeType($value,$type){
+        $result = true;
+        
+        switch($type){
+            case 'int':
+                $result = $this->analyzeInt($value);
+                $value = (int) $value;
+                break;
+        }
+        
+        return [ $value, $result ];
+    }
+    
+    /** Analiza si el valor encierra un entero valido */
+    
+    private function analyzeInt($value){
+        return preg_match('/^(\d)+$/',$value);
+    }
+    
+    public function getWebserviceData($key = null){
+        if(is_null($key)){
+            return $this->webserviceData;
+        }
+        
+        if(isset($this->webserviceData[$key])){
+            return $this->webserviceData[$key];
+        }
+        
+        return null;
+    }
+}
